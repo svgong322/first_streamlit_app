0cb657625ad2c32aca2ab6423ce9ba5207009724
svgong322 <100981922+svgong322@users.noreply.github.com>
2023-04-18 23:25:38 -0500

    Update streamlit_app.py
    Removed the Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,4 @@ streamlit.title('My Parents New Healthy Diner')
 
 My Parents New Healthy Diner
 
-Breakfast Menu
-Omega 3 & Blueberry Oatmeal
 
-Kale, Spinach & Rocket Smoothie
-
-Hard-Boiled Free-Range Egg
